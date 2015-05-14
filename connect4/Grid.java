9acbd35f63baccf59fbbeb84a22d28c9e1a6a7c1
aj-work <sethi_iitkgp@yahoo.com>
2015-05-14 16:47:39 -0500

    MiniMax in progress

--- connect4/Grid.java
+++ connect4/Grid.java
@@ -12,6 +12,8 @@ public class Grid extends JPanel implements MouseListener {
 	private Cell[][] gridUI = new Cell[6][7];
 	private static int[][] gridTrack = new int[6][7];
 	private static int player = 1;
+	private static int AI = 2;
+	private static int Human = 1;
 	private static Boolean gameOver = false;
 	public Boolean randPlayer = false;
 	private static ArrayList<Cell> cellArray = new ArrayList<Cell>();
@@ -274,9 +276,89 @@ public class Grid extends JPanel implements MouseListener {
 
 			}
 		}
-		
+
 		player = 1;
 		repaint();
 
 	}
+
+	private Move minimax(int[][] gridCopy){
+		ArrayList<Integer> xAndy = new ArrayList<Integer>();
+		Move nextMove = null;
+		int maxVal = 10000;
+		int minVal =-10000;
+		int x = 0;
+		int y = 0;
+
+		return MaxMove(gridCopy, maxVal, minVal, 3);
+		
+
+
+	}
+
+	private Move MaxMove(int[][] gridCopy, int alpha, int beta, int depth){
+		int gameValue = 0;
+		
+       Move maxMove = null,
+    		tempMove = null;
+       
+		if(depth==3){
+
+		} else {
+			for (int i = 0; i < 7; i++) {
+				tempMove = new Move(gridCopy, i, AI);
+				
+			}
+		}
+
+
+
+		return maxMove;
+
+
+
+	}
+
+	private Move MinMove(int[][] gridCopy){
+		int gameValue = 0;
+		Move minMove = null;
+		return minMove;
+
+
+
+	}
+
+
+	public class Move{
+
+		public int[][] newGrid = new int[6][7];
+		public int moveX = 9,
+				   moveY;
+
+
+
+		Move(int[][] CurrGrid, int y, int player) {
+
+			newGrid = CurrGrid;
+			for (int i = 0; i < 6 ; i++) {
+				if(CurrGrid[i][y] != 0){
+					moveX = i-1;
+					break;
+				}
+			}
+
+			if(moveX==9){
+				moveX = 5;
+			}
+
+			moveY = y;
+			newGrid[moveX][y] = player;
+
+
+		}
+
+	}
+
+
+
 }
